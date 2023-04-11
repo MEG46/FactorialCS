56e098aced1e5ea5dd4f7ed46583abf0e7048e62
Muhammed Esad Goncaloğlu <93635211+MEG46@users.noreply.github.com>
2023-04-11 07:33:33 +0300

    Add files via upload

---
+++ 3 - Factorial/Program4/Program.cs
@@ -0,0 +1,19 @@
+﻿static int fact(int num)
+{
+    if (num == 0 || num == 1)
+    {
+        return 1;
+    }
+    if (num < 0)
+    {
+        return -1;
+    }
+    return num * fact(num - 1);
+}
+while (true) {
+    int num, fac;
+    Console.Write("Please enter a number : ");
+    num = int.Parse(Console.ReadLine());
+    fac = fact(num);
+    Console.WriteLine("Factorial Of " + num + " = " + fac);
+}
